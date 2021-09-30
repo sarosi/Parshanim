ae3c3b0ce256deaa41c1344074a93985bf76e2f5
Gabor Sarosi <sarosigabor@gmail.com>
2021-10-01 00:20:50 +0200

    Initial Commit

---
+++ Parshanim/ParshanimApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParshanimApp.swift
+//  Parshanim
+//
+//  Created by Gabor Sarosi on 01.10.21.
+//
+
+import SwiftUI
+
+@main
+struct ParshanimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
